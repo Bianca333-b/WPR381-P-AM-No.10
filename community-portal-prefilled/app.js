163722ec5221a01e056737d84e30aba2773ba179
Bianca333-b <bgrobbi@icloud.com>
2025-05-08 10:31:34 +0200

    initialized git, ensured dependencies, created gitignore, and added files to git.

--- community-portal-prefilled/app.js
+++ community-portal-prefilled/app.js
@@ -11,6 +11,28 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('home'); // Looks for 'views/home.ejs'
+});
+
+app.get('/about', (req, res) => {
+  res.render('about'); // Looks for 'views/about.ejs'
+});
+
+app.get('/events', (req, res) => {
+  res.render('events'); // Looks for 'views/events.ejs'
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact'); // Looks for 'views/contact.ejs'
+});
+
+app.get('/thankyou', (req, res) => {
+  res.render('thankyou'); // Looks for 'views/thankyou.ejs'
+});
+
 app.use("/", pageRoutes);
 
 app.listen(port, () => {
--- community-portal-prefilled/routes/pageRoutes.js
+++ community-portal-prefilled/routes/pageRoutes.js
@@ -4,18 +4,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    res.render('home');
 });
 
 router.get('/about', (req, res) => {
+    res.render('about');
 });
 
 router.get('/events', (req, res) => {
+    res.render('events');
 });
 
 router.get('/contact', (req, res) => {
+    res.render('contact');
 });
 
 router.get('/thankyou', (req, res) => {
+    res.render('thankyou');
 });
 
 module.exports = router;
