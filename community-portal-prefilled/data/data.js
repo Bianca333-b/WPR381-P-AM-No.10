165508d8ef163c6f30ba08b9163be7c81618c605
Tyiaan <tiaankemp1@gmail.com>
2025-05-10 13:26:25 +0200

    Created data folder and file

---
+++ community-portal-prefilled/data/data.js
@@ -0,0 +1,25 @@
+const teamMembers = [
+  { name: "Tiaan", role: "Backend Developer" },
+  { name: "Bianca", role: "Team Lead" },
+  { name: "Yanga", role: "Frontend Developer" },
+  { name: "Vunene", role: "Data Manager" }
+];
+
+const events = [
+  {
+    title: "Community Clean-Up",
+    date: "2025-06-01",
+    location: "Central Park",
+    image: "/images/cleanup.jpg"
+  },
+  {
+    title: "Coding Bootcamp",
+    date: "2025-06-15",
+    location: "TechHub Campus",
+    image: "/images/bootcamp.jpg"
+  }
+];
+
+const contacts = [];
+
+module.exports = { teamMembers, events, contacts };
